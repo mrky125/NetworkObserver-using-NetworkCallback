2c7dcdb3a27b40903a9f65ded9992000c5fa670a
mrky125 <4ex21ia@gmail.com>
2021-02-25 19:34:53 +0900

    add display hase code of mainActivity

--- app/src/main/java/com/example/networkobserverdemo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/networkobserverdemo/viewmodel/MainViewModel.kt
@@ -3,4 +3,10 @@ package com.example.networkobserverdemo.viewmodel
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    var hashCodeMainActivity: String = ""
+
+    fun setLocalData(hashCode: Int) {
+        hashCodeMainActivity = hashCode.toString()
+    }
 }
