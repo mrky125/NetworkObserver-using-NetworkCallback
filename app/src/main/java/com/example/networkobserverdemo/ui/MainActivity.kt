f13b5650259b5ef751ec2dfa1402ce1b178443c8
mrky125 <4ex21ia@gmail.com>
2021-02-25 18:48:58 +0900

    del unnecessary code

--- app/src/main/java/com/example/networkobserverdemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/networkobserverdemo/ui/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.networkobserverdemo.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -9,22 +8,18 @@ import com.example.networkobserverdemo.R
 import com.example.networkobserverdemo.databinding.ActivityMainBinding
 import com.example.networkobserverdemo.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity(), MainViewModel.MainViewNavigator {
+class MainActivity : AppCompatActivity() {
 
-    private val viewModel = ViewModelProvider.NewInstanceFactory().create(MainViewModel::class.java)
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // set binding and viewModel
+        viewModel = ViewModelProvider.NewInstanceFactory().create(MainViewModel::class.java)
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.viewModel = viewModel
-    }
-
-    override fun onStartSubActivity() {
-        val intent = Intent(this, SubActivity::class.java)
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/networkobserverdemo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/networkobserverdemo/viewmodel/MainViewModel.kt
@@ -3,22 +3,4 @@ package com.example.networkobserverdemo.viewmodel
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-
-    interface MainViewNavigator {
-        fun onStartSubActivity()
-    }
-
-    private var navigator: MainViewNavigator? = null
-
-    fun setNavigator(navigator: MainViewNavigator) {
-        this.navigator = navigator
-    }
-
-    fun unsetNavigator() {
-        navigator = null
-    }
-
-    fun startSubActivity() {
-        navigator?.onStartSubActivity()
-    }
 }
